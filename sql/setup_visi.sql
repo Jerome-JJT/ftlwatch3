aa6f016d265428001a8f5386c94f31b8cded7d1a
Jerome Jaquemet <47446321+Jerome-JJT@users.noreply.github.com>
2023-11-15 10:56:46 +0100

    raw visibility

---
+++ sql/setup_visi.sql
@@ -0,0 +1,58 @@
+
+
+UPDATE projects SET corder=11, project_type_id = 1 WHERE slug = '42cursus-libft';
+
+UPDATE projects SET corder=21, project_type_id = 1 WHERE slug = '42cursus-ft_printf';
+UPDATE projects SET corder=22, project_type_id = 1 WHERE slug = '42cursus-get_next_line';
+UPDATE projects SET corder=23, project_type_id = 1 WHERE slug = 'born2beroot';
+
+UPDATE projects SET corder=30, project_type_id = 1 WHERE slug = 'exam-rank-02';
+UPDATE projects SET corder=31, project_type_id = 1 WHERE slug = '42cursus-push_swap';
+UPDATE projects SET corder=32, project_type_id = 1 WHERE slug = 'minitalk';
+UPDATE projects SET corder=33, project_type_id = 1 WHERE slug = 'pipex';
+UPDATE projects SET corder=34, project_type_id = 1 WHERE slug = 'so_long';
+UPDATE projects SET corder=35, project_type_id = 1 WHERE slug = '42cursus-fdf';
+UPDATE projects SET corder=36, project_type_id = 1 WHERE slug = '42cursus-fract-ol';
+
+UPDATE projects SET corder=40, project_type_id = 1 WHERE slug = 'exam-rank-03';
+UPDATE projects SET corder=41, project_type_id = 1 WHERE slug = '42cursus-minishell';
+UPDATE projects SET corder=42, project_type_id = 1 WHERE slug = '42cursus-philosophers';
+
+UPDATE projects SET corder=50, project_type_id = 1 WHERE slug = 'exam-rank-04';
+UPDATE projects SET corder=51, project_type_id = 1 WHERE slug = 'cub3d';
+UPDATE projects SET corder=52, project_type_id = 1 WHERE slug = 'minirt';
+UPDATE projects SET corder=53, project_type_id = 1 WHERE slug = 'netpractice';
+UPDATE projects SET corder=54, project_type_id = 1 WHERE slug = 'cpp-module-00';
+UPDATE projects SET corder=56, project_type_id = 1 WHERE slug = 'cpp-module-01';
+UPDATE projects SET corder=57, project_type_id = 1 WHERE slug = 'cpp-module-02';
+UPDATE projects SET corder=58, project_type_id = 1 WHERE slug = 'cpp-module-03';
+UPDATE projects SET corder=59, project_type_id = 1 WHERE slug = 'cpp-module-04';
+
+UPDATE projects SET corder=60, project_type_id = 1 WHERE slug = 'exam-rank-05';
+UPDATE projects SET corder=61, project_type_id = 1 WHERE slug = 'cpp-module-05';
+UPDATE projects SET corder=62, project_type_id = 1 WHERE slug = 'cpp-module-06';
+UPDATE projects SET corder=63, project_type_id = 1 WHERE slug = 'cpp-module-07';
+UPDATE projects SET corder=64, project_type_id = 1 WHERE slug = 'cpp-module-08';
+UPDATE projects SET corder=65, project_type_id = 1 WHERE slug = 'cpp-module-09';
+UPDATE projects SET corder=66, project_type_id = 1 WHERE slug = 'ft_containers';
+UPDATE projects SET corder=67, project_type_id = 1 WHERE slug = 'inception';
+UPDATE projects SET corder=68, project_type_id = 1 WHERE slug = 'webserv';
+UPDATE projects SET corder=69, project_type_id = 1 WHERE slug = 'ft_irc';
+
+UPDATE projects SET corder=70, project_type_id = 1 WHERE slug = 'exam-rank-06';
+UPDATE projects SET corder=71, project_type_id = 1 WHERE slug = 'ft_transcendence';
+
+
+
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2013.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2014.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2015.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2016.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2017.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2018.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2019.%';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" LIKE '2020.%';
+
+UPDATE poolfilters SET hidden=TRUE WHERE "name" = '2021.june';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" = '2022.april';
+UPDATE poolfilters SET hidden=TRUE WHERE "name" = 'None.None';
