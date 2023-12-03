e7832c858ae0a2c61836b0c4d7dba3176551d762
Jeroma <jerome.jaquemet@gmail.com>
2023-12-03 15:45:59 +0100

    fix event key

--- site/src/components/Events/EventsPage.tsx
+++ site/src/components/Events/EventsPage.tsx
@@ -31,7 +31,7 @@ export function EventPage(): JSX.Element {
 
   function EventCard(card: any): JSX.Element {
     return (
-      <Card id={card.login} key={card.login} className="flex min-w-96 w-96 max-w-96 border-black border-2">
+      <Card id={card.id} key={card.id} className="flex min-w-96 w-96 max-w-96 border-black border-2">
         <CardHeader floated={false} title={card.name} className='flex justify-center shadow-none mx-2 mt-2 bg-black/10 my-text truncate'>
           {card.name}
         </CardHeader>
