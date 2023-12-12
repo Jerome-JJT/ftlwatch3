6e1ce2a0637d051ac730a87545298a006205db4d
Jeroma <jerome.jaquemet@gmail.com>
2023-12-12 07:51:08 +0100

    index correctly per page

--- site/src/components/Common/SuperTable.tsx
+++ site/src/components/Common/SuperTable.tsx
@@ -323,7 +323,7 @@ export function SuperTable({
                       <td key={`${createKey(value, index)}-index`}
                         className={classNames('border-x border-blue-gray-50 overflow-hidden p-4 max-w-4 table-cell')}>
                         <div className="h-full flex justify-center items-center text-black">
-                          {index + 1}
+                          {(currentPage - 1) * usersPerPage + (index + 1)}
                         </div>
                       </td>
                     }
