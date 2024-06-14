7dba207310f2471f2e3381553ca2666651e99e9f
Jerome-JJT <47446321+Jerome-JJT@users.noreply.github.com>
2024-06-14 12:20:35 +0200

    fix 2023 fix fix

--- api/update_users.py
+++ api/update_users.py
@@ -381,7 +381,7 @@ def import_users(longway=True):
                                         AVG(correction_point) AS avgpoints, 
                                         AVG(wallet) AS avgwallets
                                         FROM users
-                                        WHERE (blackhole > NOW() OR active = TRUE OR grade = 'Member') AND hidden = FALSE""")[0]
+                                        WHERE (blackhole > NOW() OR is_active = TRUE OR grade = 'Member') AND hidden = FALSE""")[0]
 
         mylogger(f"End users worker, for {infos['nbusers']}, {infos['sumpoints']} points (moy {infos['avgpoints']}), moy wallets {infos['avgwallets']}", LOGGER_ALERT)
     
