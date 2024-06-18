1ab8f54cbc9572eaa700115b02099e40983be840
Jeroma <jerome.jaquemet@gmail.com>
2024-06-18 13:56:56 +0200

    bundle optimization

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -4,5 +4,18 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    minify:        true,
+    cssMinify:     true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          framer:   ['framer-motion'],
+          gauge:    ['react-gauge-chart'],
+          tailwind: ['@material-tailwind/react'],
+        },
+      },
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 });
