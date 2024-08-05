21639bc2bb92488e1751f78212e4adc503707430
Jeroma <jerome.jaquemet@gmail.com>
2024-08-05 23:49:16 +0200

    int to str

--- api/update_teams.py
+++ api/update_teams.py
@@ -96,7 +96,7 @@ def team_notification(fetched):
     if leader["avatar_url"] != None:
         embed["thumbnail"] = f'{leader["avatar_url"]}'
 
-    embed['title'] = f'{fetched["status"]}{(" "+fetched["final_mark"]) if fetched["final_mark"] != None else ""} for {leader["login"]}, on {project["slug"] if project != None else ""}'
+    embed['title'] = f'{fetched["status"]}{(" "+str(fetched["final_mark"])) if fetched["final_mark"] != None else ""} for {leader["login"]}, on {project["slug"] if project != None else ""}'
     if (len(refer) == 0):
         refer = None
     else:
