ffc702de55bf03f8046aa8135161e0ba361e3128
Jerome Windows <jerome.jaquemet@gmail.com>
2026-04-12 10:42:04 +0200

    f4

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './main.css';
 // import { ThemeProvider } from '@material-tailwind/react';
-import { NotificationProvider } from '.components/Notifications/NotificationsProvider.tsx';
+import { NotificationProvider } from './components/Notifications/NotificationsProvider.tsx';
 import { LoginProvider } from './components//Hooks/LoginProvider.tsx';
 import { ThemeProvider } from '@material-tailwind/react';
 import { theme } from './tailwind.theme.ts';
